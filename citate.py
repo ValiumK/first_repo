bd69daaeb7fe28c5e34df5aac885faf09728d4d5
Konstantin Poslanichenko <swarthevdi@gmail.com>
2021-08-24 20:09:46 +0300

    Add file citate.py

---
+++ citate.py
@@ -0,0 +1,7 @@
+first_name = " albert  "
+last_name = " einstein  "
+first_name = first_name.strip()
+last_name = last_name.strip()
+famous_person = f"{first_name} {last_name}"
+message = f'{famous_person.title()} once said:\n\t"A person who never made mistake, never tried another new."'
+print(message)
