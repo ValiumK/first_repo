95e043c91a4e9fba2fb3b9e9b169477328aa1bd0
Konstantin Poslanichenko <swarthevdi@gmail.com>
2021-08-25 21:26:23 +0300

    Add country_list.py and pizza_animal(for).py

---
+++ country_list.py
@@ -0,0 +1,9 @@
+# 25.08.2021 Упражнение с сортировкой списков
+countries =  ['vietnam', 'syria', 'usa', 'spain', 'yemen']
+len(countries)
+print(sorted(countries, reverse=True))
+countries.reverse()
+countries.reverse()
+countries.sort()
+countries.sort(reverse=True)
+print(countries)
---
+++ pizza_animal(for).py
@@ -0,0 +1,10 @@
+# 25.08.2021 Упражнение с цилом for
+pizzas = ['chili', 'hunter', 'hawaii']
+for pizza in pizzas:
+	print(f"I like {pizza.title()} pizza!") 
+print("I really love pizza!")
+
+animals = ['dog', 'cat', 'alligator']
+for animal in animals:
+	print(f"A {animal.title()} would make a great pet!")
+print("Any of these animals would make a great pet!")
